00d54aa5f6809771693969c0abdd06d62c6e0bae
Pavol Juhas <pavol.juhas@gmail.com>
2013-06-04 18:12:38 -0400

    added a dummy file

---
+++ py03import/exelmath/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# Directory can be imported under its name if it contains a __init__.py
+# file.  The __init__.py file is executed on the module import.  It can
+# be empty or it can be used to import function from other files in the
+# directory.  The Python files in the directory are not loaded unless
+# called from __init__.py or imported explicitly with "import dir1.file".
+
+# The first string in the __init__.py file is a docstring for the directory
+# package.
+
+"""Simple math functions.
+"""
+
+# Exercise: import the solveQuadraticRoots function here so it can be
+# be imported directly from exelmath.
+
+# FIXME ... some import statement
