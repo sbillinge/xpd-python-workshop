f06de5552b10f777aa608b5aecee0f92392a2c8b
Pavol Juhas <pavol.juhas@gmail.com>
2013-06-07 00:35:10 -0400

    added the isStringTrue function.

--- py04exceptunit/stringchecks.py
+++ py04exceptunit/stringchecks.py
@@ -31,3 +31,17 @@ def isfloat(s):
     'Return True if string can be converted to a floating point value'
     # +++your code here+++
     return
+
+def isStringTrue(s):
+    '''Translate string to its logical value.
+
+    Return True or False.
+    Raise ValueError if the string is not understood.
+    '''
+    if s == "yes":
+        return True
+    elif s == "no":
+        return False
+    else:
+        emsg = 'String must be either "yes" or "no".'
+        raise ValueError(emsg)
