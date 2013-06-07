994b170c459ce17263e50b3b53f7ece05a9a21ea
Pavol Juhas <pavol.juhas@gmail.com>
2013-06-06 23:02:10 -0400

    Added the first exceptions exercise.

---
+++ py04exceptunit/stringchecks.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+# INSTRUCTIONS:
+#
+# (1) add parent directory to a pth file so that "import py04exceptunit" works.
+#
+# (2) execute in a terminal (or command prompt):
+#       python -m py04exceptunit.teststringchecks
+#     This will perform unit tests for this file.
+#
+# (3) Edit this file and repeat step (2) until all tests pass
+
+
+"Functions that check if string is convertible to another type.
+"
+
+def isint(s):
+    'Return True if string can be converted to an integer.'
+    # +++your code here+++
+    return
+
+
+def isfloat(s):
+    'Return True if string can be converted to a floating point value'
+    # +++your code here+++
+    return
