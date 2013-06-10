c8dcdcd6eed3bb0e3fe0232ada79ae2c41f5cde5
Pavol Juhas <pavol.juhas@gmail.com>
2013-06-10 03:10:40 -0400

    second exercise for a plot of an ellipse

---
+++ py06matplotlib/ex02ellipse.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+# INSTRUCTIONS:
+#
+# (1) Plot ellipse with the main axes a=2, b=1.  Make sure the ellipse is
+#     drawn at correct aspect ratio.
+#
+# (2) Export the figure in EPS format to an ex02ellipse.eps file.
+#
+# HINT: Check documentation for matplotlib functions "axis" and "savefig".
+# ----------------------------------------------------------------------------
+
+"Plot of an ellipse rendered in correct scale."
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# clear the current figure
+plt.clf()
+
+phi = np.linspace(0, 2 * np.pi, 400)
+# +++your code here+++
+
+# redraw and display the figure window
+plt.draw()
+plt.show()
