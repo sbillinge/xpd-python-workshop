16dba5c0c7bda34f018eecd735bdcc500eb50eb6
Pavol Juhas <pavol.juhas@gmail.com>
2013-06-10 12:52:58 -0400

    added skeleton for the phone application.

---
+++ py07oop/textmessage.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env
+
+# INSTRUCTIONS:
+# 
+# (1) Implement the TextMessage class accordig to docstring specifications.
+
+
+class TextMessage(object):
+    '''TextMessage between two smart phones.
+
+    Instance Attributes:
+
+    sender    -- SmartPhone instance that is sending the message
+    recipient -- SmartPhone instance that receives the message
+    message   -- text message string
+    '''
+
+    def __init__(self, sender, recipient, message):
+        '''Construct a text message object.
+
+        sender    -- SmartPhone instance that is sending the message
+        recipient -- SmartPhone instance that receives the message
+        message   -- text message string
+        '''
+        # +++your code here+++
+        pass
+
+
+    def __str__(self):
+        '''String representation of the text message formatted as:
+
+        sender_name: message text
+
+        Return string.
+        '''
+        # +++your code here+++
+        pass
