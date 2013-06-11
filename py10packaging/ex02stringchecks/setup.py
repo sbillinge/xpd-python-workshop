6011172fca8ec9e22df5dbf0ea3bd6b83e459221
Pavol Juhas <pavol.juhas@gmail.com>
2013-06-11 13:45:59 -0400

    Files for the Python distribution exercise.

---
+++ py10packaging/ex02stringchecks/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+# INSTRUCTIONS:
+#
+# Edit setup.py function below to create working distribution.
+
+"""ex02stringchecks -- library for checking if string can be converted
+to numeric and logical values.
+"""
+
+from distutils.core import setup
+
+setup(name='FIXME',
+      version='FIXME',
+      description='FIXME',
+      author='FIXME',
+      author_email='FIXME',
+      url='http://FIXME ... URL to this directory on your Google Code clone',
+      packages=[],      # Python modules to be included in the distribution
+      data_files=[],    # non-python files to be included in the distribution
+     )
